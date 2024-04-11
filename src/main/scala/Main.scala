f5aeac997ffc585cb7fc27944fe3bd954c690ca8
luizaki <francinelouise.sanchez@gmail.com>
2024-04-11 23:30:56 +0800

    Initializing files

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+@main def main(): Unit =
+    // login() system for admin
+        // after logging in, display menu
+
+    // menu()
+    println("Bakery Inventory Management System")
+    // options for
+      // displaying items (already with potential calculation of spoilage/other shit or separate option nlng sya)
+        // pwede rin side thing lng un
+      // adding/deleting items
+
+    // other key stuff to remember
+      // classes for Item, which will be inherited by Product and Ingredient classes?
+      // main.scala shall only contain the menu system, separate files for other functions (maybe)
