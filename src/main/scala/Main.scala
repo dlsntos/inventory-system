5efbd5a2efe213f15852b664a94f0afcd0b3a908
SDGY <154787650+dlsntos@users.noreply.github.com>
2024-04-13 18:22:37 +0800

    updated_main_1
    - added temporary menu for login and main screen
    - added inputs for admin login (pass code can be changed to Admin ID )and choices
    - added conditionals after entering  choice
    - functions for the chosen choices will be created soon
    - exceptions will be added later

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,42 @@
+import scala.io.StdIn.readLine // Scanner for string values
+import scala.io.StdIn.readInt  // Scanner for integer values
+// Prints will be updated later
 @main def main(): Unit =
+    println("Bakery Inventory Management System\n")//SDGY
     // login() system for admin
-        // after logging in, display menu
+    println("[1] Login\n")//SDGY
+    println("[2] Exit\n")
+    print("Enter choice: "); val choice = readInt()
+    
+    //SDGY
+    if choice == 1 then// Update login
+      print("Enter Admin name: "); val username = readLine()
+      print("Enter Admin Numeric passcode: "); val passcode = readInt()
 
-    // menu()
-    println("Bakery Inventory Management System")
+      //add error handling later
+      //after logging in, display menu
+      println("Inventory Menu\n\n")// menu()
+      println("[1] Show Pastries") //shows 
+      println("[2] Add new baked pastry")
+      println("[3] remove spoiled pastry")
+      //other functions to be added
+      println("[4] Exit\n")
+      
+      print("Enter choice: "); val choice = readInt()
+      // functions will be added later
+        if choice == 1 then
+          println()//function
+        if choice == 2 then
+          println()//function
+        if choice == 3 then
+          println()//function
+        if choice == 4 then
+          println("Exit")
+    if choice == 2 then
+      print("Exit")//Edit exit message later
+    
+    
+     
     // options for
       // displaying items (already with potential calculation of spoilage/other shit or separate option nlng sya)
         // pwede rin side thing lng un
