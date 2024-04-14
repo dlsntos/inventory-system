f5169aafa6c2188d2c9e64b49ae787979d435ce3
SDGY <154787650+dlsntos@users.noreply.github.com>
2024-04-14 10:45:45 +0800

    Update Item.scala

--- src/main/scala/Item.scala
+++ src/main/scala/Item.scala
@@ -1,1 +1,7 @@
-class Item
+abstract class Item:
+    
+    def menu(): Unit
+    def displayItems(): Unit // display items
+    def addItem(): Unit  // add an item to inventory
+    def removeItem(): Unit // remove item from inventory
+    def calculateSpoilage(): Unit //calculate spoilage?
