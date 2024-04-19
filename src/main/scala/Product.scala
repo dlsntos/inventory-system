0a1ca7e38dec3eea2d0c671c26e310f6d44adc51
luizaki <francinelouise.sanchez@gmail.com>
2024-04-20 00:22:34 +0800

    Overhaul product structure

--- src/main/scala/Product.scala
+++ src/main/scala/Product.scala
@@ -1,17 +1,33 @@
-import scala.collection.mutable.ArrayBuffer
+import java.util.Date
+class Product:
+    /** A product existing in the inventory.
+     * 
+     * @param name of the product
+     * @param price unit price of the product
+     * @param unit unit measurement (eg. pieace, kg)
+     * @param limit maximum quantity in the inventory
+     * @param quantity current amount of item
+     * @param expiration number of days before spoilage
+     * @param items list of items (instances) of this product
+     */
 
-class Product extends Item:
-    /* Product is a child of Item, specifying ingredients used to make product and a unit price
-    it is being sold for */
-
-    private var _ingredients = new ArrayBuffer[String]
+    private var _name: String = ""
     private var _price: Double = 0.0
+    private var _unit: String = ""
+    private var _limit: Int = 0
+    private var _quantity: Int = 0
+    private var _expiration: Int = 0
+    private var _items: List[Item] = Nil
 
-    // setters and getters
-    def ingredients: ArrayBuffer[String] = _ingredients
-    def ingredients_=(newIngredients: ArrayBuffer[String]) =
-        _ingredients = newIngredients
-    
-    def price: Double = _price
-    def price_=(newPrice: Double) =
-        _price = newPrice
+    def addItem(item: Item): Unit =
+        _items = item :: _items
+
+    class Item():
+        /** Instance of a particular product.
+         * 
+         * @param id randomly generated unique identifier
+         * @param expirationDate specific date of expiration, calculated with product expiration
+         */
+
+        private var _id: String = ""
+        private var _expirationDate: Date = ""
